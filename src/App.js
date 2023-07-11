4813f090ea66fe9c63fca9974ea88e5cf2041731
joualighar@gmail.com <joualighar@gmail.com>
2023-07-11 13:07:43 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useEffect,useState } from "react";
 const App = () => {
+  const [users,setUsers]=useState([])
+  useEffect(()=>{
+    const getUsers = async()=>{
+      const response = await fetch('http://localhost:3001/users')
+      const data = await response.json()
+      setUsers(data)
+    }
+    getUsers()
+  },[])
   return (
     <>
       <p>bienvenue mes amis a cette formation react</p>
+      {
+        users && users.map(user=>(
+          <p key={user.id}>{user.nom} {user.age}</p>
+        ))
+      }
     </>
   );
 }
