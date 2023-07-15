05a0af44ba595703648050bd884f705ac58e3ee0
gharib196702 <joualighar@gmail.com>
2023-07-15 18:48:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import { useEffect,useState } from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Home from "./composants/Home";
+import Products from "./composants/Products";
+import {useState} from "react";
+
 const App = () => {
-  const [users,setUsers]=useState([])
-  useEffect(()=>{
-    const getUsers = async()=>{
-      const response = await fetch('http://localhost:3001/users')
-      const data = await response.json()
-      setUsers(data)
-    }
-    getUsers()
-  },[])
+  const [currentRoute, setCurrentRoute] = useState()
+  const handleClick = () => {
+    const path = window.location.path
+    console.log(path)
+
+  }
   return (
     <>
-      <p>listes des programmeurs </p>
-      {
-        users && users.map(user=>(
-          <p key={user.id}>{user.nom} {user.age}</p>
-        ))
-      }
+      <BrowserRouter>
+        <nav>
+          <ul className="nav nav-pills">
+            <li onClick={()=>setCurrentRoute('home')}>
+              <Link className={currentRoute==='home'?'btn btn-info':'btn btn-outline-info'} to="/home">Home</Link>
+            </li>
+            <li onClick={()=>setCurrentRoute('produits')}>
+              <Link className={currentRoute==='produits'?'btn btn-info':'btn btn-outline-info'} to="/produits">Produits</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home"
+            element={<Home/>}/>
+          <Route path="/produits"
+            element={<Products/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  );
+  )
+
 }
 
 export default App;
